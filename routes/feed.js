454b852940ca0235d09aaef0124094de996f1a24
Sona <sonatumasyan1@gmail.com>
2021-07-29 18:26:40 +0400

    transforming async await

--- routes/feed.js
+++ routes/feed.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { body } = require('express-validator');
 const feedController = require('../controllers/feed');
 const {check} = require('../middleware/auth');
+
 const router = express.Router();
 
 // GET /feed/posts
 router.get('/posts', check, feedController.getPosts);
+
 
 // POST /feed/post
 router.post('/post', [
