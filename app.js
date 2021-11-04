af38fe22d79a69d9c5b0a45a8bbb2d8a6bbea1ac
Art <esoyan.2020@mail.ru>
2021-11-04 15:47:57 +0400

    comments already deleted

--- app.js
+++ app.js
@@ -1,50 +1,11 @@
 const path = require('path');
-
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
 const authRoutes = require('./routes/auth');
-
-
-
-
-
-// const fs = require('fs');
-//
-// function someAsyncOperation(callback) {
-//     // Assume this takes 95ms to complete
-//     fs.readFile('/path/to/file', callback);
-// }
-//
-// const timeoutScheduled = Date.now();
-//
-// setTimeout(() => {
-//     const delay = Date.now() - timeoutScheduled;
-//     console.log(`${delay} ms have passed since I was scheduled`);
-// }, 100);
-//
-// // do someAsyncOperation which takes 95 ms to complete
-// someAsyncOperation(() => {
-//     const startCallback = Date.now();
-//     // do something that will take 10ms...
-//     while (Date.now() - startCallback < 10) {
-//         // do nothing
-//     }
-// });
-
-
-// timeout_vs_immediate.js
-setTimeout(() => {
-    console.log('timeout');
-}, 0);
-
-setImmediate(() => {
-    console.log('immediate');
-});
-
 
 
 
